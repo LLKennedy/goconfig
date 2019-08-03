faa9e0270ed39f570dd7a3f7da1ed748d6f19270
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-08-03 22:40:06 +1000

    Add readme

--- load.go
+++ load.go
@@ -11,5 +11,6 @@ var envGetter = os.Getenv
 // Load loads the default config location on the provided file system, returning defaults for any keys not present in the config file.
 // Options start as defaults, then load from environment variables, then a config file, then runtime flags, each overriding the previous.
 func Load(empty interface{}, flags map[string]interface{}, fs vfs.FileSystem, envAccess func(string) string) (opts interface{}, err error) {
+	opts = empty
 	return
 }
